b5555cef12f01f6c0900ebf169e93c3b4bb2c533
Cameron Canning <cameroncanning.1@gmail.com>
2021-12-19 16:14:04 -0500

    file structure updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
-import './App.css';
-import SearchBox from './SearchBox';
+import SearchBox from './components/SearchBox';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 const axios = require('axios');
 
 function App() {
@@ -13,19 +15,34 @@ function App() {
 		.then(res => setChain(res.data.chain));
 	}
 
-	return (
-		<div className="App">
-			<SearchBox
-				setPage={setStart}/>
-			<SearchBox 
-				setPage={setEnd}/>
-			<button onClick={getChain}>get</button>
-			<div>
-				{chain.reduce((prev, curr) => prev += ' ' + curr, '')}
-			</div>
-			
-		</div>
-  	);
+	const isMobile = window.innerWidth <= 500;
+	if (!isMobile) {
+		return (
+				<Container fluid>
+					<Row>
+						<p>Chain</p>
+						{chain.reduce((prev, curr) => prev += ' ' + curr, '')}
+					</Row>
+					<Row>
+						<Col>
+							<SearchBox
+								page={start}
+								setPage={setStart}/>
+						</Col>
+						<Col>
+							<button onClick={getChain}>get</button>
+						</Col>
+						<Col>
+							<SearchBox 
+								page={end}
+								setPage={setEnd}/>
+						</Col>
+					</Row>
+				</Container>
+		)}
+	else {
+		return <p>mobile</p>
+	}
 }
 
 export default App;
